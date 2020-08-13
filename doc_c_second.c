b5228cc65d3475188f1252a1ba86a20018ea0706
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-13 18:28:36 +0900

    p.117 - 04-01.c

--- doc_c_second.c
+++ doc_c_second.c
@@ -3,19 +3,17 @@
 
 int main(void) {
 
-	int nInput = 0;
-	printf("정수를 입력하세요.: ");
+	int nInput1 = 0;
+	int nInput2 = 0;
+	printf("두 정수를 입력하세요.: ");
 
 	//사용자로부터 정수를 입력받는다.
-	scanf("%d", &nInput);
-		if (nInput == 0) {
-			printf("0으로 나눌수는 없습니다. 종료합니다.\n");
-			return 0; 
-	}
+	scanf("%d%d", &nInput1,&nInput2);
+	
 	//만일 사용자가 0을 입력한다면 10을 0으로 나눠야 한다.
-	printf("%d\n", 10 / nInput);
+	printf("AVG : %.2f\n", (nInput1+nInput2)/2.0 );
 	return 0;
 }
 /*/================================//
- p.116 - opdividebyzero.c
+ p.117 - 04-01.c
 //================================/*/
