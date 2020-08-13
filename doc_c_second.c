7f1a5b86d129fbead5d327dc590c24dce7b434b7
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-13 19:20:51 +0900

    p.127 - opassign02.c

--- doc_c_second.c
+++ doc_c_second.c
@@ -2,24 +2,12 @@
 #pragma warning (disable: 4996) 
 
 int main(void) {
-	int nResult = 0, nData = 10;
+	int x = 0, y = 0;
 
-	nResult += 3; //nResult = nResult + 3;
-	printf("%d\n", nResult);
-
-	nResult *= nData;		//nResult = nResult * nData;
-	printf("%d\n", nResult);
-
-	nResult /= 2;			//nResult = nResult / 2;
-	printf("%d\n", nResult);
-
-	nResult -= nData - 5;	//nResult = nRessult -(nData - 5);
-	printf("%d\n", nResult);
-
-	nResult %= 3;			//nResult = nResult % 3 ;
-	printf("%d\n", nResult);
+	x = y = 10;
+	printf("%d, %d\n", x,y );
 	return 0;
 }
 /*/================================//
- p.126 - opcomassign01.c
+ p.127 - opassign02.c
 //================================/*/
