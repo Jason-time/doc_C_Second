374382db27934e7a2f407c5c09b8689c22ff7524
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-13 16:55:32 +0900

    p.099 - scanfage02.c

--- doc_c_second.c
+++ doc_c_second.c
@@ -3,22 +3,24 @@
 
 int main(void) {
 
-	char sszName[32] = { 0 };
+	char szName[32] = { 0 };
 	int nAge = 0;
 
-	//사용자로부터 정수를 입력받아 nAge 변수에 저장한다.
-	printf("나이를 입력하세요.  : ");
-	scanf("%d", &nAge);
+	//사용자로부터 나이를 입력받는다.
+	//이때, 입력의 완료를 의미하는 <Enter> 키(\n)입력이 버퍼에 남는다.
+	printf("나이를 입력하세요. : ");
+	scanf_s("%d%*c", &nAge);
 
-	//이름 중간에 빈 공백이 있더라도 문제없이 입력할 수 있다. 
 	printf("이름을 입력하세요. : ");
-	gets(sszName);
+	//버퍼 안에 남아있는 것들을 모두 비운 후 이름을 입력받는다.
+	//따라서 개행문자(\n)가 제거된다.
+	//fflush(stdin);
+	//__fpurge(stdin);
+	gets_s(szName, sizeof(szName));
 
-	//이름과 나이를 출력한다.
-	printf("%d, %s\n", nAge, sszName);
-
+	printf("%d, %s\n", nAge, szName);
 	return 0;
 }
 /*/================================//
- p.097 - scanfage01.c
+ p.099 - scanfage02.c
 //================================/*/
