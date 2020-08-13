7dbd4d34e648309f85fb1836ac1dda641537723d
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-13 19:01:28 +0900

    p.121 - opassignconst01.c

--- doc_c_second.c
+++ doc_c_second.c
@@ -2,16 +2,17 @@
 #pragma warning (disable: 4996) 
 
 int main(void) {
+	char szBuffer[32] = { 0 };
 
-	int x = 0, nInput = 0;
-	scanf("%d", &nInput);
+	//상수에 대입 연산을 수행할 수 없다. 
+	3 = 4;
+	//배열의 이름은 '주소상수'다. 변수가 아니다. 
+	szBuffer = 'A';
 
-	//nInput 변수에 저장된 사용자가 입력한 값을 x에 복사한다.
-	//x에 들어있던 값은 Overwrite되고 사라진다.
-	x = nInput;
-	printf("%d\n", x);
+
+
 	return 0;
 }
 /*/================================//
- p.120 - opassign01.c
+ p.121 - opassignconst01.c
 //================================/*/
