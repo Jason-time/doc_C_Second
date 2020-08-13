d6a457a6a64ffae52220d85084bb5e8c5e968e80
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-13 19:18:29 +0900

    p.126 - opcomassign01.c

--- doc_c_second.c
+++ doc_c_second.c
@@ -2,19 +2,24 @@
 #pragma warning (disable: 4996) 
 
 int main(void) {
-	//nTemp는 '교환'을 위해 필요한 변수이다.
-	int x = 10, y = 20, nTmp = 0;
-	printf("Before: x(%d), y(%d)\n", x, y);
+	int nResult = 0, nData = 10;
 
-	//두 변수 x와 y에 저장된 값을 서로 교환한다. 
-	nTmp = x;
-	x = y;
-	y = nTmp;
+	nResult += 3; //nResult = nResult + 3;
+	printf("%d\n", nResult);
 
-	//교환이 완료된 것을 확인하기 위해 출력한다.
-	printf("After: x(%d), y(%d)", x, y);
+	nResult *= nData;		//nResult = nResult * nData;
+	printf("%d\n", nResult);
+
+	nResult /= 2;			//nResult = nResult / 2;
+	printf("%d\n", nResult);
+
+	nResult -= nData - 5;	//nResult = nRessult -(nData - 5);
+	printf("%d\n", nResult);
+
+	nResult %= 3;			//nResult = nResult % 3 ;
+	printf("%d\n", nResult);
 	return 0;
 }
 /*/================================//
- p.123 - opswap1.c
+ p.126 - opcomassign01.c
 //================================/*/
