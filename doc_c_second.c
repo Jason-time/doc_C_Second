85bbd1946d6d8eb0acf62083451d3ea48922f372
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-13 17:20:28 +0900

    p.112 - operator02.c

--- doc_c_second.c
+++ doc_c_second.c
@@ -5,13 +5,12 @@ int main(void) {
 
 	int nResult = 0;
 
-	//1: 3+4
-	//2: 7(임시결과) -5
-	//3: nResult =2
-	nResult = 3 + 4 - 5;
-	printf("nResult : %d\n", nResult);
+	//임시결과 7을 변수에 담아둔다.
+	nResult = 3 + 4;
+	//변수에 담아둔 정보를 다시 꺼내 연산한다.
+	printf("nResult : %d\n", nResult - 5);
 	return 0;
 }
 /*/================================//
- p.110 - operator01.c
+ p.112 - operator02.c
 //================================/*/
