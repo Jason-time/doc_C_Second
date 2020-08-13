094cb7c02bab2f952c120297ec05d8ed60d5498e
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-13 15:49:30 +0900

    p.095 - scanfstring01.c

--- doc_c_second.c
+++ doc_c_second.c
@@ -3,12 +3,15 @@
 
 int main(void) {
 
-	char ch = 0;
-	scanf("%c", &ch);
-	printf("입력한 문자는 %c 입니다.\n", ch);
+	//문자열을 저장하기 위한 배열 선언 및 정의
+	char szBuffer[32] = { 0 };
+
+	//사용자가 입력한 문자열을 배열에 저장하고 출력
+	scanf("%s", szBuffer);
+	printf("%s\n", szBuffer);
 
 	return 0;
 }
 /*/================================//
- p.094 - scanfchar01.c
+ p.095 - scanfstring01.c
 //================================/*/
