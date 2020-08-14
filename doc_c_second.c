8be73f830224765455cdf9123b0cc0668f1f0a49
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-14 12:33:12 +0900

    p.158 - oplogicalerror01.c

--- doc_c_second.c
+++ doc_c_second.c
@@ -6,11 +6,11 @@ int main(void) {
 	int nInput = 0, bResult = 0;
 
 	scanf("%d", &nInput);
-	bResult = nInput > 3 && nInput < 20;
+	bResult =  3 < nInput < 20;
 	printf("Result : %d (1:True, 0:False)\n", bResult);
 
 	return 0;
 }
 /*================================================
- p.157 - oplogical02.c  논리곱(AND)
+ p.158 - oplogicalerror01.c  
 ==================================================/*/
