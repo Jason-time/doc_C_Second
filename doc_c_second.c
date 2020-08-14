e9751a3e200def92f7ff8e1a3866fbba771ebddf
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-14 15:59:24 +0900

    p.186 - 6.1 if문을 이용한 최댓값 구하기

--- doc_c_second.c
+++ doc_c_second.c
@@ -2,23 +2,23 @@
 #pragma warning (disable: 4996) 
 int main(void) {
 
-	int nAge = 0;
+	int nInput = 0,nMax=0;
 
-	printf("나이를 입력하세요. : ");
-	scanf("%d", &nAge);
+	scanf("%d", &nInput);
+	if (nMax < nInput)
+		nMax = nInput;
 
-	//if문의 시작
-	if (nAge >= 20) {
+	scanf("%d", &nInput);
+	if (nMax < nInput)
+		nMax = nInput;
 
-		//조건식을 만족했을 때 실행할 구문들
-		//여러 구문을 실행해야 한다면 반드시 블록 스코프로 묶는다.
-		printf("처리 전, 당신의 나이는 %d세 입니다.\n", nAge);
-		nAge = 20;
-	}//<- if문의 끝
+	scanf("%d", &nInput);
+	if (nMax < nInput)
+		nMax = nInput;
 
-	printf("당신의 나이는 %d세 입니다.\n", nAge);
+	printf("MAX : %d", nMax);
 	return 0;
 }
 /*================================================
- p.185 - ctrlbranch02.c	6.1.2 제어문과 스코프
+ p.186 - 	6.1 if문을 이용한 최댓값 구하기
 ==================================================/*/
