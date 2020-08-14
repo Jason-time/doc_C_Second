12afc2ac5825c27e4da862b6fe21943cd01e0d1d
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-14 15:20:07 +0900

    p.171 - 세 정수 중에서 가장 큰 수 구하기(서바이벌 방식)

--- doc_c_second.c
+++ doc_c_second.c
@@ -1,15 +1,22 @@
 ﻿#include<stdio.h>
 #pragma warning (disable: 4996) 
 int main(void) {
-	int nMax = 0;
-	int a, b, c;
 
-	scanf("%d%d%d", &a, &b, &c);
-	nMax = a > b ? a : b>c?b:c;
+	int nMax=-100, nInput = 0;
+
+	scanf("%d", &nInput);
+
+	nMax = nInput > nMax ?  nInput : nMax;
+
+	scanf("%d", &nInput);
+	nMax = nInput > nMax ? nInput : nMax;
+
+	scanf("%d", &nInput);
+	nMax = nInput > nMax ? nInput : nMax;
 
 	printf("MAX : %d\n", nMax);
 	return 0;
 }
 /*================================================
- p.168 - 세 정수 중에서 가장 큰 수 구하기
+ p.171 - 세 정수 중에서 가장 큰 수 구하기(서바이벌 방식)
 ==================================================/*/
