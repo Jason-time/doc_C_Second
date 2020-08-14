05aa74fa5752701d59d323e01e364df0eaf51117
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-14 12:47:26 +0900

    p.163 - opcondi01.c 5.4 조건 연산자(삼항 연산자)

--- doc_c_second.c
+++ doc_c_second.c
@@ -1,22 +1,16 @@
 ﻿#include<stdio.h>
 #pragma warning (disable: 4996) 
-
 int main(void) {
+	
+	int nInput = 0, nSelect = 0;
+	scanf("%d", &nInput);
 
-	int nAge = 0, nHeight = 0;
-
-	printf("나이를 입력하세요. : ");
-	scanf("%d", &nAge);
-	printf("키를 입력하세요. : ");
-	scanf("%d", &nHeight);
-	//모든 논리 연산은 왼쪽에서 오른쪽으로 진행하지요.
-	//만일 논리적으로 더 수행할 필요가 없다면
-	//수행하지 않은 나머지 연산은 무시합니다.
-	printf("결과 : %d (1:합격, 0:불합격)\n",
-		nAge >= 20 && nAge <= 30 && nHeight >= 150);
+	//조건식이 참이면 nSelect =10이 되고 거짓이면 nSelect =20이 된다.
+	nSelect = nInput <= 10 ? 10 : 20;
+	printf("%d\n", nSelect);
 
 	return 0;
 }
 /*================================================
- p.160 - opshortcircuit01.c 쇼트서킷 
+ p.163 - opcondi01.c 5.4 조건 연산자(삼항 연산자)
 ==================================================/*/
