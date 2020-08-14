e8002dbb8d47b3d08ff391c8ff19aef4e04b243d
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-14 12:26:31 +0900

    p.155 - oplogical01.c 논리합(OR)과 논리곱(AND)

--- doc_c_second.c
+++ doc_c_second.c
@@ -3,13 +3,14 @@
 
 int main(void) {
 
-	printf("%d\n", 300 == 299.99999f);
-	printf("%d\n", 300 == 299.99999);
-	
-	printf("%d\n", 300 == 299.9999);
-	printf("%d\n", 300 == 300.00001F);
+	int nInput = 0, bResult = 0;
+
+	scanf("%d", &nInput);
+	bResult = nInput < 4 || nInput >= 60;
+	printf("Result : %d (1:True, 0:False)\n", bResult);
+
 	return 0;
 }
-/*=================================
- p.153 - oprelintfloat01.c sizeof 연산자
-==================================/*/
+/*================================================
+ p.155 - oplogical01.c 논리합(OR)과 논리곱(AND)
+==================================================/*/
