35b849eb4fb42b3751032778fda9bf72d8b085f7
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-14 14:19:48 +0900

    p.168 - 세 정수 중에서 가장 큰 수 구하기

--- doc_c_second.c
+++ doc_c_second.c
@@ -1,13 +1,15 @@
 ﻿#include<stdio.h>
 #pragma warning (disable: 4996) 
 int main(void) {
-	int nHeight = 0;
-	printf("키를 입력하세용 : ");
-	scanf_s("%d", &nHeight);
-	nHeight >= 150 ? printf("결과 : 합격") : printf("결과 : 불합격");
+	int nMax = 0;
+	int a, b, c;
 
+	scanf("%d%d%d", &a, &b, &c);
+	nMax = a > b ? a : b>c?b:c;
+
+	printf("MAX : %d\n", nMax);
 	return 0;
 }
 /*================================================
- p.166 - 합격자와 불합격자를 분류하는 방법
+ p.168 - 세 정수 중에서 가장 큰 수 구하기
 ==================================================/*/
