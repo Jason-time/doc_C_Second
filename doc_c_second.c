ae11bcbafb07ae6e0d4b15aae015354ae4f6bd7a
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-14 19:27:09 +0900

    p.204 - 단계별 분류에 따른 버스요금 계산

--- doc_c_second.c
+++ doc_c_second.c
@@ -2,25 +2,26 @@
 #pragma warning (disable: 4996) 
 int main(void) {
 
-	int nCharge = 1000;
+	int nFee= 1000;
 	int nAge = 0;
 	double nRate = 0.0;
 	scanf("%d", &nAge);
 
-	if(nAge<14)
+	if(nAge<=19)
 	{
-		if (nAge < 4)nRate = 0;
-		else
-			nRate = 0.5;
+		nRate = 0.25;
+		if (nAge>=4 && nAge<=13)nRate = 0.5;
+		else if(nAge<4)
+			nRate = 1.0;
 	}
 	else {
-		if (nAge < 20) nRate = 0.75;
+		if (nAge >= 20 && nAge<65) nRate = 0.0;
 		else nRate = 1.0;
 	}
 
-	printf("최종요금 : %.0f원", nCharge*nRate);
+	printf("최종요금 : %.0f원", nFee-nFee*nRate);
 	return 0;
 }
 /*================================================
- p.197 - 나이에 따른 분류 및 요금계산
+ p.204 - 단계별 분류에 따른 버스요금 계산
 ==================================================/*/
