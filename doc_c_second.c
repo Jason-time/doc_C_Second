6f2bba7394634db6b97bd4e83b18141c80dfc690
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-14 13:26:37 +0900

    p.166 - 합격자와 불합격자를 분류하는 방법

--- doc_c_second.c
+++ doc_c_second.c
@@ -1,18 +1,13 @@
 ﻿#include<stdio.h>
 #pragma warning (disable: 4996) 
 int main(void) {
-	
-	int nInput = 0, nSelect = 0;
-	scanf("%d", &nInput);
-
-	//nInput <= 10 ? nSelect = 10 : nSelect=20 ; l-value 오류
-	//printf("%d\n", nSelect);
-
-	nInput <= 10 ? (nSelect = 10) : (nSelect=20) ;
-	printf("%d\n", nSelect);
+	int nHeight = 0;
+	printf("키를 입력하세용 : ");
+	scanf_s("%d", &nHeight);
+	nHeight >= 150 ? printf("결과 : 합격") : printf("결과 : 불합격");
 
 	return 0;
 }
 /*================================================
- p.164 - opcondierror01.c 5.4 조건 연산자(삼항 연산자)
+ p.166 - 합격자와 불합격자를 분류하는 방법
 ==================================================/*/
