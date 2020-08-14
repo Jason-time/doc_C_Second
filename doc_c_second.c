58f55f922dc702b38a749c5cff7ff19295a10feb
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-14 13:16:54 +0900

    p.164 - opcondierror01.c 5.4 조건 연산자(삼항 연산자)

--- doc_c_second.c
+++ doc_c_second.c
@@ -5,12 +5,14 @@ int main(void) {
 	int nInput = 0, nSelect = 0;
 	scanf("%d", &nInput);
 
-	//조건식이 참이면 nSelect =10이 되고 거짓이면 nSelect =20이 된다.
-	nSelect = nInput <= 10 ? 10 : 20;
+	//nInput <= 10 ? nSelect = 10 : nSelect=20 ; l-value 오류
+	//printf("%d\n", nSelect);
+
+	nInput <= 10 ? (nSelect = 10) : (nSelect=20) ;
 	printf("%d\n", nSelect);
 
 	return 0;
 }
 /*================================================
- p.163 - opcondi01.c 5.4 조건 연산자(삼항 연산자)
+ p.164 - opcondierror01.c 5.4 조건 연산자(삼항 연산자)
 ==================================================/*/
