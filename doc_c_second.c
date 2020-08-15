a05544282ea5c7eb53e3e8703b078b43630ab0fc
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-15 22:21:41 +0900

    p.308 arraycross01.c 지그재그로 2차원 배열 채우기

--- doc_c_second.c
+++ doc_c_second.c
@@ -2,27 +2,28 @@
 #pragma warning (disable: 4996) 
 int main(void) {
 
-	int aList[4][2][3] = { 0 };
-	int i = 0, j = 0, k = 0, nCounter = 0;
+	int aList[5][5] = { 0 };
+	int i = 0, j = 0, nCounter = 0;
 
-	for (i = 0;i < 4;++i) {
-		//면
-		printf("Plane number : %d\n", i);
-		for (j = 0;j < 2;++j) {
-			//행
-			for (k = 0;k < 3;++k) {
-				//열
-				aList[i][j][k] = ++nCounter;
-				printf("%d\t", aList[i][j][k]);
-			}
-			putchar('\n');
-		}
-		printf("\n\n");
+	for (i = 0;i < 5;++i) {
+		if (i % 2 == 0)
+			for (j = 0;j < 5;++j)
+				aList[i][j] = ++nCounter;
+		else
+			for (j = 4;j >= 0;--j)
+				aList[i][j] = ++nCounter;
 	}
+
+	for (i = 0; i < 5;i++) {
+		for (j = 0; j < 5; ++j)
+			printf("%d\t", aList[i][j]);
+		putchar('\n');
+	}
+
 	return 0;
 }
 
 /*================================================
- p.286 array05.c / 8.4.2 3차원 배열
+ p.308 arraycross01.c 지그재그로 2차원 배열 채우기
 ==================================================/*/
 
