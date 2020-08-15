c8265ed2e1cc20f361f549b3fb4aeab083453687
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-15 18:14:05 +0900

    p.272 08-01 배열에서 최댓값 구하기

--- doc_c_second.c
+++ doc_c_second.c
@@ -2,18 +2,23 @@
 #pragma warning (disable: 4996) 
 int main(void) {
 
-	int nInput=-1;
+	int aList[5] = { 30,40,10,50,20 };
+	int i = 0;
 
-	while (nInput < 0 || nInput > 10)
-	{
-		printf("Input number : ");
-	scanf("%d", &nInput);
+	//여기에 들어갈 코드를 작성합니다.
+	for (i = 1; i < 5;i++) {
+		if (aList[0] < aList[i])
+			aList[0] = aList[i];
 	}
-	puts("END");
+	//이하 코드는 수정하지 않습니다.
+	for (i = 0;i < 5;++i)
+		printf("%d\t", aList[i]);
+	putchar('\n');
 
+	printf("MAX: %d\n", aList[0]);
 	return 0;
 }
 /*================================================
- p.242 07-07 피라미드 출력하기
+ p.272 08-01 배열에서 최댓값 구하기
 ==================================================/*/
 
