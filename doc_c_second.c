083c1f1e95b1428eae2db5333aae7f9f5074b166
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-15 18:22:14 +0900

    p.277 arraystring02.c / 8.3.2 문자열의 끝이 '\0'인 이유

--- doc_c_second.c
+++ doc_c_second.c
@@ -2,26 +2,22 @@
 #pragma warning (disable: 4996) 
 int main(void) {
 
-	int aList[5] = { 30,40,10,50,20 };
-	int i = 0,nTmp=0;
+	char szBuffer[32] = { 0 };
+	int nLength = 0;
 
-	//여기에 들어갈 코드를 작성합니다.
-	for (i = 1; i < 5;i++) {
-		if (aList[0] > aList[i]) {
-			nTmp = aList[i];
-			aList[i] = aList[0];
-			aList[0] = nTmp;
-		}
-	}
-	//이하 코드는 수정하지 않습니다.
-	for (i = 0;i < 5;++i)
-		printf("%d\t", aList[i]);
-	putchar('\n');
+	//이름을 입력받아 배열에 저장한다.
+	printf("Input your name : ");
+	gets(szBuffer);
 
-	printf("MIN: %d\n", aList[0]);
+	//배열의 시작부터 '\0'가 나올 때까지 계속 다음으로 넘기고 확인한다.
+	while (szBuffer[nLength] != '\0')
+		nLength++;
+
+	//이름과 문자열의 길이를 출력한다.
+	printf("Your name is %s(%d). \n", szBuffer, nLength);
 	return 0;
 }
 /*================================================
- p.273 08-02 요소의 값을 교환하는 방식으로 배열에서 최솟값 구하기
+ p.277 arraystring02.c / 8.3.2 문자열의 끝이 '\0'인 이유
 ==================================================/*/
 
