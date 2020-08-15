f8ec4b1c3b9d8965db936c84e5209004984a30d7
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-15 13:26:16 +0900

    p.241 07-05 첫 번째 직각 삼각형 출력하기

--- doc_c_second.c
+++ doc_c_second.c
@@ -1,19 +1,19 @@
 ﻿#include<stdio.h>
 #pragma warning (disable: 4996) 
 int main(void) {
-	int nInput1 = 0, nInput2 = 0;
-	scanf("%d", &nInput1);
-	if (nInput1 > 9 || nInput1 < 2)
-		puts("ERROR");
-	else {
-	while (nInput2 < 9) {
-		printf("%d * %d = %d\n", 
-			nInput1, ++nInput2, nInput1 * nInput2);
-		}
+
+	int nStar = 0,nLevel=0,i=0;
+	for (i;i < 5;++i)
+	{
+
+		for (nStar = 0;nStar < i + 1;nStar++)
+			printf("* ");
+		puts("");
 	}
+
 	return 0;
 }
 /*================================================
- p.237 07-04 구구단에서 한 단만 출력하기
+ p.241 07-05 첫 번째 직각 삼각형 출력하기
 ==================================================/*/
 
