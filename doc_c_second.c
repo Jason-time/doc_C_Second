b8a8e763ba3fa8f51b5f479a478023ea4af5ccda
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-15 18:48:38 +0900

    p.286 array05.c / 8.4.2 3차원 배열

--- doc_c_second.c
+++ doc_c_second.c
@@ -2,30 +2,27 @@
 #pragma warning (disable: 4996) 
 int main(void) {
 
-	int aList[3][4] = {
-		{10,20,30},
-		{40,50,60}
-	};
-	int i = 0, j = 0;
+	int aList[4][2][3] = { 0 };
+	int i = 0, j = 0, k = 0, nCounter = 0;
 
-	//여기에 들어갈 코드를 작성합니다.
-	for (i = 0;i < 2;i++)
-		for (j = 0;j < 3;j++)
-			aList[i][3] += aList[i][j];
-
-	for (j = 0;j < 4;j++)
-		for (i = 0;i < 2;i++)
-			aList[2][j] += aList[i][j];
-
-	for (i = 0;i < 3;++i) {
-		for (j = 0;j < 4;++j)
-			printf("%d\t", aList[i][j]);
-		putchar('\n');
+	for (i = 0;i < 4;++i) {
+		//면
+		printf("Plane number : %d\n", i);
+		for (j = 0;j < 2;++j) {
+			//행
+			for (k = 0;k < 3;++k) {
+				//열
+				aList[i][j][k] = ++nCounter;
+				printf("%d\t", aList[i][j][k]);
+			}
+			putchar('\n');
+		}
+		printf("\n\n");
 	}
 	return 0;
 }
 
 /*================================================
- p.282 08-03 / 2차원 배열의 행과 열의 총합 계산하기
+ p.286 array05.c / 8.4.2 3차원 배열
 ==================================================/*/
 
