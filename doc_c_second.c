9304f23b08436507508e196f2a6ba357694aaed8
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-15 12:50:22 +0900

    p.234 - 5행 5열의 사각형 출력하기

--- doc_c_second.c
+++ doc_c_second.c
@@ -1,20 +1,20 @@
 ﻿#include<stdio.h>
 #pragma warning (disable: 4996) 
 int main(void) {
+	int nInput1 = 0, nInput2 = 0;
 
-	char ch = 0;
-	int nIndex = 0;
-
-	while ((ch = getchar()) != '\n') {
-		
-		//버퍼에서 꺼낸 문자의 인덱스와 값을 출력한다.
-		printf("%02d\t%c\n", nIndex, ch);
-		//한 글자를 출력했으니 인덱스도 1 증가시킨다.
-		++nIndex;
+	while (nInput1 < 5) {
+		nInput2 = 0;
+		while (nInput2 < 5) {
+			printf("*\t");
+			nInput2++;
+		}
+		nInput1++;
+		puts("\n");
 	}
 	return 0;
 }
 /*================================================
- p.228 - ctrlwhilevariable01.c / 7.1.3 반복문 내부에 선언한 자동변수
+ p.234 - 5행 5열의 사각형 출력하기
 ==================================================/*/
 
