37fe78e76bd54b448ada1b4c5ae5b00db0aebe80
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-17 13:32:08 +0900

    p.315 arraylookup01.c/ 9.4 Lookup 배열

--- doc_c_second.c
+++ doc_c_second.c
@@ -1,58 +1,37 @@
 ﻿#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
 #pragma warning (disable: 4996) 
 
-int PrintMenu(void) {
-	int nInput = 0;
-
-	//메뉴를 출력하기 전에 화면을 지운다. (Clear screen)
-	system("cls");
-	//메뉴를 출력하고 사용자 입력을 받는다.
-	printf("[1]New\t[2]Search\t[3]Print\t[4]Remove\t[0]Exit\n:");
-//	scanf("%d", &nInput);
-
-	//메뉴를 선택하자 말자 반응하게 한다.
-	//입력값을 문자로 인식하기 때문에 숫자로 바꾸기 위해 48을 빼줘야 한다.
-	nInput=	_getch();
-	nInput -= 48;
-
-	//사용자가 선택한 메뉴 값을 반환한다.
-	return nInput;
-}
-
 int main(void) {
-	int  nMenu = 0;
-	//메뉴 이벤트 루프
-	//사용자가 입력한 값에 따라 정해진 기능을 반복해 수행한다.
-	//(0을 입력하면 반복문을 끝낸다.
-	while ((nMenu = PrintMenu()) != 0)
-	{
-		switch (nMenu) {
-		case 1: //New
-			puts("새 항목을 추가합니다.");
-			break;
-		case 2:	//Search
-			puts("기존 항목에서 검색합니다.");
-			break;
-		case 3:	//Print
-			puts("전체 내용을 축력합니다.");
-			break;
-		case 4:	//Remove
-			puts("기존 항목을 삭제합니다.");
-			break;
-		default:	//???
-			puts("\n알 수 없는 명령입니다.\n1-4를 입력하세요.(종료:0)");
-		}
-		_getch();
-	}
-	puts("Bye~~");
+	//전체 경우에 대한 과금률 결과를 담ㅇ든 배열의 선언 및 정의
+	double aRate[10] = {
+		0.0,0.1,0.25,	//1~3세 0%,10%,25%
+		0.5,0.5,		//4~5세 50%
+		0.6,0.65,		//6~7세 60%,65%
+		0.8, 0.82, 0.97	//8~10세 80%,82%,97%
+	};
+	int nAge = 0, i = 0, nFee = 1000;
+
+	//요금표를 출력한다.
+	printf("요금표\n");
+	for (i = 1;i <= 10;++i)
+		printf("%2d세 요금 : \t%3d원\n", i, (int)(nFee * aRate[i - 1]));
+	putchar('\n');
+
+	//나이를 입력받고 배열 인덱스 범위 내부로 값을 강제 조정한다.
+	printf("나이를 입력하세요. : ");
+	scanf("%d", &nAge);
+	if (nAge < 1)		nAge = 1;
+	else if (nAge > 10)	nAge = 10;
+
+	//나이에 맞는 배열요소를 선택하고 별다른 베어문 없이 즉시 값을 출력한다.
+	printf("최종요금 : %d원\n", (int)(nFee * aRate[nAge - 1]));
+	putchar('\n');
 
 	return 0;
 
 }
 
 /*================================================
-p.341 Printmenu01.c	/ 메뉴 이벤트 루프 구현
+p.315 arraylookup01.c	/ 9.4 Lookup 배열
 ==================================================/*/
 
