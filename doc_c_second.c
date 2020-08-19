61c9a930616b8fa7325ce0aee0ffe21b7ee987df
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-19 18:12:53 +0900

    p.404 ptrnarray02.c  / 11.2.5 배열 연산자 풀어쓰기

--- doc_c_second.c
+++ doc_c_second.c
@@ -4,23 +4,31 @@
 
 int main(void) {
 
-	char szBuffer[32] = { "I am a boy." };
+	char szBuffer[32] = { "You are a girl." };
 
-	//배열의 주소를 출력한다.
-	printf("%p\n", szBuffer);
+	//배열의 첫 번째(0번) 요소의 값을 %c 형식으로 출력한다.
+	printf("%c\n", szBuffer[0]);
+	//0번 요소에 대한 주소인  배열의 이름(주소)에 대해 간접지정 연산을
+	//수행하고 그 안에 담긴 정보를 출력한다.
+	printf("%c\n", *szBuffer);
+	//0을 더하더라도 주소는 달라지지 않는다.
+	printf("%c\n", *(szBuffer + 0));
 
-	//대상 문자열에서 문자열을 검색하고 찾은 위치(주소)를 출력한다.
-	printf("%p\n", strstr(szBuffer, "am"));
-	printf("%p\n", strstr(szBuffer, "boy"));
+	//배열 연산자는 '기준주소 + 인덱스' 연산겨려과로 얻은 주소를
+	//간접지정한 것과 같다.
+	printf("%c\n", szBuffer[5]);
+	printf("%c\n", *(szBuffer + 5));
 
-	//문자열이 검색된 위치에서 기준이 되는 주소를 빼면
-	//인덱스를 계산할 수 있다. 
-	printf("Index: %d\n", strstr(szBuffer, "am") - szBuffer);
-	printf("Index: %d\n", strstr(szBuffer, "boy") - szBuffer);
+	//주소연산(&)은 간접지정 연산과 상반된다.
+	//그러므로 아래 세 줄의 코드는 모두 같다.
+	printf("%s\n", &szBuffer[4]);
+	printf("%s\n", &*(szBuffer + 4));
+	printf("%s\n", szBuffer + 4);
+
 	return 0;
 }
 
 /*================================================
-p.401 ptrsearch01.c  / 11.2.4 문자열 검색 
+p.404 ptrnarray02.c  / 11.2.5 배열 연산자 풀어쓰기
 ==================================================/*/
 
