828d13bf1d6bfb467aa72f363b444f1131400151
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-19 13:31:33 +0900

    p.374 pointer02.c / 간접 지정 연산자.

--- doc_c_second.c
+++ doc_c_second.c
@@ -3,19 +3,25 @@
 
 int main(void) {
 
-	int nData = 10;
-	printf("%s\n", "nData");
+	// int 형식 변수 선언 및 정의
+	int x = 10;
+	//변수 x를 가리키는 int 형식에 대한 포인터 변수 선언 및 정의
+	int* pnData = &x;
 
-	//변수 ndata에 들어 있는 값 출력
-	printf("%d\n", nData);
-	//변수 nData의 메모리 주소를 출력
-	printf("0x%p\n", &nData);
+	printf("x : %d\n", x);
+
+	//pnData 포인터 변수가 가리키는 대상 메모리를 int형 변수로
+	//간접 지정하고 20을 대입한다.
+	//현재 가리키는 대상 메모리는 변수 x의 메모리이므로 x의 값이 20이 된다. 
+	*pnData = 20;
+	printf("x : %d\n", x);
+	printf("%s\n", "nData");
 
 	return 0;
 
 }
 
 /*================================================
-p.370 pointer01.c / 주소 번지 연산자
+p.374 pointer02.c / 간접 지정 연산자.
 ==================================================/*/
 
