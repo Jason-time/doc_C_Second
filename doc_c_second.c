77f9fce153bea609440b1ef36dab770ce76c5781
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-19 15:02:09 +0900

    p.379 ptrstring01.c

--- doc_c_second.c
+++ doc_c_second.c
@@ -1,25 +1,36 @@
 ﻿#include<stdio.h>
 #pragma warning (disable: 4996) 
+#include<string.h>
 
 int main(void) {
 
-	// int 배열 선언 및 정의. 배열의 이름은 연속된 각 요소들 중
-	//전체를 대표하는 0번째 요소에 대한 '주소 상수'이다.
-	int aList[5] = { 0 };
-	//int에 대한 포인터 변수를 배열의 이름으로 정의한다.
-	int* pnData = aList;
+	// 문자 배열(char[16])의 선언 및 정의
+	// 선언한 크기는 char[16]이지만 초기화는 char[6] 문자열로 한다.
 
-	//배열의 0번 요소의 값을 출력한다.
-	printf("aList[0] : %d\n", aList[0]);
+	char szBuffer[16] = { "Hello" };
 
-	//포인터가 가리키고 있는 배열의 0번 요소의 값을 번경하고 출력한다.
-	*pnData = 20;
-	printf("aList[0] : %d\n", aList[0]);
+	//문자 배열을 가리키는 문자 포인터 변수의 선언 및 정의
+	char* pszData = szBuffer;
+	int nLength = 0;
+
+	//pszData 포인터 변수가 가리키는 대상에 저장된 char형 데이터가
+	//문자열의 끝을 의미하는 NULL문자가 될 때까지 반복문 수행
+	while (*pszData != '\0') {
+
+		//포인터를 다음으로 한 칸 이동 시킨다!!
+		pszData++;
+		nLength++;
+	}
+
+	//strlen() 함수로 문자열의 길이(바이트 단위 크기)를 출력한다.
+	printf("Length : %d\n", nLength);
+	printf("Length : %d\n", strlen(szBuffer));
+	printf("Length : %d\n", strlen("world"));
 	return 0;
 
 }
 
 /*================================================
-p.377 ptrnarray01.c / 11.1.3 포인터와 배열
+p.379 ptrstring01.c  
 ==================================================/*/
 
