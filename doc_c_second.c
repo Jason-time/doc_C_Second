21feabea5ac019988e2cd02f8a8f1b4b61b50b17
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-19 13:26:33 +0900

    p.370 pointer01.c / 주소 번지 연산자

--- doc_c_second.c
+++ doc_c_second.c
@@ -1,41 +1,21 @@
 ﻿#include<stdio.h>
 #pragma warning (disable: 4996) 
-int GetData(void){
-	int nInput = 0;
-	printf("숫자를 입력하세요. : ");
-	scanf("%d", &nInput);
-	return nInput;
-}
 
-int GetMax(int a,int b, int c){
-	int nMax = a;
-	if (b > nMax)	nMax = b;
-	if (c > nMax)	nMax = c;
-	return nMax;
-}
-
-void PrintData(int a, int b, int c, int nMax) {
-	printf("%d, %d, %d, 중 가장 큰 수는 %d 입니다.\n", a, b, c, nMax);
-}
 int main(void) {
 
-	int aList[3] = { 0 };
-	int nMax  = -9999, i = 0;
-	//입력
-	for (i = 0;i < 3;++i)
-		aList[i] = GetData();
+	int nData = 10;
+	printf("%s\n", "nData");
 
-	//최대값 계산
-	nMax = GetMax(aList[0], aList[1], aList[2]);
-
-	//출력
-	PrintData(aList[0], aList[1], aList[2], nMax);
+	//변수 ndata에 들어 있는 값 출력
+	printf("%d\n", nData);
+	//변수 nData의 메모리 주소를 출력
+	printf("0x%p\n", &nData);
 
 	return 0;
 
 }
 
 /*================================================
-p.347 10-01 / 사용자가 입력한 값에서 최댓값 구하기와 코드 분할
+p.370 pointer01.c / 주소 번지 연산자
 ==================================================/*/
 
